b80ab2d24faa558802c000fe28a52e5abed528d2
Oleksandr <pogrebnyak19999@gmail.com>
2023-08-23 17:34:39 +0200

    Add fixes

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 // const hamburger = document.querySelector('.hamburger'),
 //     menu = document.querySelector('.menu'),
 //     closeElem = document.querySelector('.menu__close');
-  
+
 // hamburger.addEventListener('click', () => {
 //     menu.classList.add('active');
 // });
@@ -17,33 +17,53 @@
 
 $(document).ready(function () {
   $("a[href]").on("click", function (event) {
-  event.preventDefault();
-  var id = $(this).attr("href"),
-  top = $(id).offset().top;
-  $("body,html").animate({ scrollTop: top }, 1500);
+    event.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1500);
   });
-}); 
+});
 
 $("img.img-svg").each(function () {
-    var $img = $(this);
-    var imgClass = $img.attr("class");
-    var imgURL = $img.attr("src");
-    $.get(
-      imgURL,
-      function (data) {
-        var $svg = $(data).find("svg");
-        if (typeof imgClass !== "undefined") {
-          $svg = $svg.attr("class", imgClass + " replaced-svg");
-        }
-        $svg = $svg.removeAttr("xmlns:a");
-        if (!$svg.attr("viewBox") && $svg.attr("height") && $svg.attr("width")) {
-          $svg.attr(
-            "viewBox",
-            "0 0 " + $svg.attr("height") + " " + $svg.attr("width")
-          );
-        }
-        $img.replaceWith($svg);
-      },
-      "xml"
-    );
+  var $img = $(this);
+  var imgClass = $img.attr("class");
+  var imgURL = $img.attr("src");
+  $.get(
+    imgURL,
+    function (data) {
+      var $svg = $(data).find("svg");
+      if (typeof imgClass !== "undefined") {
+        $svg = $svg.attr("class", imgClass + " replaced-svg");
+      }
+      $svg = $svg.removeAttr("xmlns:a");
+      if (!$svg.attr("viewBox") && $svg.attr("height") && $svg.attr("width")) {
+        $svg.attr(
+          "viewBox",
+          "0 0 " + $svg.attr("height") + " " + $svg.attr("width")
+        );
+      }
+      $img.replaceWith($svg);
+    },
+    "xml"
+  );
+});
+
+function servicesTabs() {
+  $(".services").each(function () {
+    let ths = $(this);
+    ths.find(".services__content").not(":first").hide();
+    ths
+      .find(".services__tab")
+      .click(function () {
+        ths
+          .find(".services__tab")
+          .removeClass("services__tab_active")
+          .eq($(this).index())
+          .addClass("services__tab_active");
+        ths.find(".services__content").hide().eq($(this).index()).fadeIn();
+      })
+      .eq(0)
+      .addClass("active");
   });
+}
+servicesTabs();
