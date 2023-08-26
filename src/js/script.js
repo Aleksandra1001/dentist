1e3cb58640e923f2d8d4237565ede219344ee5ac
Aleksandra1001 <allandra1029384756@gmail.com>
2023-08-26 11:11:07 +0200

    add overlay

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,3 @@
-// const hamburger = document.querySelector('.hamburger'),
-//     menu = document.querySelector('.menu'),
-//     closeElem = document.querySelector('.menu__close');
-
-// hamburger.addEventListener('click', () => {
-//     menu.classList.add('active');
-// });
-// closeElem.addEventListener('click', () => {
-//     menu.classList.remove('active');
-// });
-
-// const percents = document.querySelectorAll('.skills__rating-percent'),
-//     rectangles = document.querySelectorAll('.skills__rating-rectangle span');
-// percents.forEach( (item, i) => {
-//     rectangles[i].style.width = item.innerHTML;
-// });
-
 $(document).ready(function () {
   $("a[href]").on("click", function (event) {
     event.preventDefault();
@@ -116,3 +99,9 @@ $(document).ready(function () {
     return false;
   });
 });
+
+$(".modal__button").on("click", function () {
+  $(".overlay, #thanks").fadeOut("slow");
+});
+
+
